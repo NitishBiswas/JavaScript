66d559878afe804d6f6f499240f1a27be7ffe1cf
Nitish Biswas <55139122+NITISHBISWAS@users.noreply.github.com>
2021-06-08 03:12:56 +0600

    Update script.js

--- JavaScript Basic/Functions/script.js
+++ JavaScript Basic/Functions/script.js
@@ -58,3 +58,43 @@ function discount(disc, ...prices) {
 const res = discount(0.2, 40, 30, 20);
 
 console.log(res);
+
+
+// Default parameters
+
+function interest1(pricipal, rate, years) {
+    rate = rate || 3.5;
+    years = years || 5;
+    return pricipal * rate / 100 * years;
+}
+
+console.log(interest1(10000));
+
+function interest2(pricipal, rate = 3.5, years = 5) {
+    return pricipal * rate / 100 * years;
+}
+
+console.log(interest2(10000));
+
+// Getters Setters method
+
+const person = {
+    firstName: 'Nitish',
+    lastName: 'Biswas',
+
+    get fullName() {
+        return `${this.firstName} ${this.lastName}`
+    },
+
+    set fullName(value) {
+        const parts = value.split(' ');
+        this.firstName = parts[0];
+        this.lastName = parts[1];
+    }
+}
+
+console.log(person.fullName);
+
+person.fullName = 'Hey, Nitish_Biswas';
+
+console.log(person.fullName);
